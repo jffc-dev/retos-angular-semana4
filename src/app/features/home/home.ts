9294c1f93c2617315087c611ae72c2ccd2278552
Javier Flores <jffc.dev@gmail.com>
2026-08-19 22:17:39 -0500

    agregar redireccion con botones

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  private router = inject(Router)
+
+  redireccionar(ruta: string){
+    this.router.navigate([ruta])
+  }
+}
